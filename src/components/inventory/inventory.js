570cda9e2bdfb5f8f0b14a01e564c43bb28b2e82
modesnero <modesnero@gmail.com>
2021-01-29 18:49:50 +0300

    add background & titles

--- src/components/inventory/inventory.js
+++ src/components/inventory/inventory.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react'
 import './inventory.scss'
 
 export default class inventory extends Component {
-  render() {
+  render () {
     return (
-      <div className="inventory">
-        <div className="main-block">
-          
+      <div className='inventory-page'>
+        <div className='top-panel'>
+          <div className="title">Экипировка</div>
+          <div className="title">Инвентарь</div>
+          <div className="button-exit">
+            <div className="btn">ESC</div>
+            Закрыть
+          </div>
         </div>
+        <div className='main-block'>
+          <div className='equipment'></div>
+          <div className='inventory'></div>
+          <div className='inventory-func'></div>
+        </div>
+        <div className='bot-panel'></div>
       </div>
     )
   }
