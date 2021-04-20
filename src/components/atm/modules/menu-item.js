67916965726b2dac3659bfc610bb05305880aa22
modesnero <modesnero@gmail.com>
2021-04-21 02:30:31 +0300

    1 more test commit, I am testing WebStorm :)

--- src/components/atm/modules/menu-item.js
+++ src/components/atm/modules/menu-item.js
@@ -5,16 +5,17 @@ import MenuIcon from '../svg/menu-icon'
 
 export default observer(({ data: { title, text, id } }) => {
   const [isHover, setHover] = useState(false)
+  const { setCurrentPage } = store
   return (
     <div
       className='menu__item'
-      onClick={() => store.setCurrentPage(title)}
+      onClick={() => setCurrentPage(title)}
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
     >
       <div className='item__title'>{title}</div>
       <div className='item__text'>{text}</div>
-      <MenuIcon num={id} active={isHover} />
+      <MenuIcon num={id} active={isHover}/>
     </div>
   )
 })
