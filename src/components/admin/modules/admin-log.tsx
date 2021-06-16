daa4744026e6e9ce7bf4a76589b07db0c3eddd59
nevainero <nevainero@gmail.com>
2021-06-16 05:37:30 +0300

    feature(admin): add empty tsx & scss components

---
+++ src/components/admin/modules/admin-log.tsx
@@ -0,0 +1,10 @@
+import React        from 'react'
+import { observer } from 'mobx-react-lite'
+
+export const AdminLog = observer(() => {
+  return (
+    <div className='admin-log'>
+
+    </div>
+  )
+})
---
+++ src/components/admin/modules/chat.tsx
@@ -0,0 +1,10 @@
+import React        from 'react'
+import { observer } from 'mobx-react-lite'
+
+export const Chat = observer(() => {
+  return (
+    <div className='chat'>
+
+    </div>
+  )
+})
